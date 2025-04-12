4b50b44d0c8ea9bdf2187d0812c8777927f2876f
EricQiao <ericqiao2001@gmail.com>
2025-04-12 17:22:30 +0800

    It could run again

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,6 +1,7 @@
 // src/i18n/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import en from '../../public/locales/en/translation.json';
 import id from '../../public/locales/id/translation.json';
 import ms from '../../public/locales/ms/translation.json';
@@ -18,14 +19,19 @@ const resources = {
 };
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'en', // Default language
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
     },
+    detection: {
+      order: ['localStorage', 'navigator', 'htmlTag'],
+      caches: ['localStorage'],
+    },
   });
+
 
 export default i18n;
